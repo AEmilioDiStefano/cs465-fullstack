9bb2d3d0be67eec79f8b7a46521c48d9f6a5d4a6
unknown <aemiliodistefano@gmail.com>
2025-04-02 07:03:35 -0600

    Module 6 work almost finished

---
+++ app_admin/src/app/data/trips.ts
@@ -0,0 +1,39 @@
+export const trips = [
+    {
+        "code" : "CENT251031",
+        "name" : "Centralia",
+        "length" : "2 nights / 3 days",
+        "start" : "2025-10-31T08:00:00Z",
+        "resort" : "Upstate Pennsylvania, 3 stars",
+        "perPerson" : "249.00",
+        "image" : "centralia-town.jpg",
+        "description" : "<p>Visit central Pennsylvania's most charming entrance to the underworld.  Enjoy the misty screen of smoke which seeps lazily through deep fissures which lead to an inferno unimaginable to the human mind.  DISCLAIMER: Travlr Getaways will not be held liable for injury or death resulting from lycanthropy, zombification, general posession, or interaction with local fauna." 
+    } , {
+        "code" : "GALR210214",
+        "name" : "Gale Reef",
+        "length" : "4 nights / 5 days",
+        "start" : "2021-02-14T08:00:00Z",
+        "resort" : "Emerald Bay, 3 stars",
+        "perPerson" : "799.00",
+        "image" : "reef1.jpg",
+        "description" : "<p>Gale Reef is a reef named Gale.  This description is being rendered dynamically from a JSON file!" 
+    } , {
+        "code" : "DAWR210315",
+        "name" : "Dawson's Reef",
+        "length" : "4 nights / 5 days",
+        "start" : "2021-03-15T08:00:00Z",
+        "resort" : "Blue Lagoon, 4stars",
+        "perPerson" : "1199.00",
+        "image" : "reef2.jpg",
+        "description" : "<p>Dawson's Reef is kind of like Gale reef, but its name is Dawson.  This description is being rendered dynamically from a JSON file!" 
+    } , {
+        "code" : "CLAR210621",
+        "name" : "Claire's Reef",
+        "length" : "4 nights / 5 days",
+        "start" : "2021-06-21T08:00:00Z",
+        "resort" : "Coral Sands, 5 stars",
+        "perPerson" : "1999.00",
+        "image" : "reef3.jpg",
+        "description" : "<p>Claire's Reef is another option like the others, but its name is Claire.  This description is being rendered dynamically from a JSON file!" 
+    }
+]
